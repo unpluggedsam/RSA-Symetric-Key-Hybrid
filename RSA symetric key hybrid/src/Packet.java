9dabc00c4309c6efd27aea92ed695732bf0cb09f
Sam <unpluggedsam990@gmail.com>
2023-01-19 21:34:10 -0800

    add keySize instance field

--- RSA symetric key hybrid/src/Packet.java
+++ RSA symetric key hybrid/src/Packet.java
@@ -2,11 +2,13 @@ public class Packet {
 
     private final int[] binary;
     private final int[] key;
+    private final OneTimePad.KEY_SIZE size;
 
     public Packet(int[] binary, int[]
-            key) {
+            key, OneTimePad.KEY_SIZE size) {
         this.binary = binary;
         this.key = key;
+        this.size = size;
     }
 
     public int[] interceptBinary() {
@@ -15,5 +17,9 @@ public class Packet {
 
     public int[] interceptKey() {
         return key;
+    }
+
+    public OneTimePad.KEY_SIZE interceptKeySize() {
+        return size;
     }
 }
