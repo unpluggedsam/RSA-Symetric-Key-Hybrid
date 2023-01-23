d7d55df63198f6c0fc40aefefa21936348614c0c
Sam <unpluggedsam990@gmail.com>
2023-01-23 10:32:00 -0800

    rename package

---
+++ RSA symetric key hybrid/src/Main.java
@@ -0,0 +1,49 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String text = "I LOVE eating some stuff occasionally and having friends.";
+
+        Computer computer1 = new Computer();
+        Computer computer2 = new Computer();
+
+        Packet packet = computer1.sendMessage(computer2, text, OneTimePad.KEY_SIZE.ONE_HUNDRED_TWENTY_EIGHT);
+
+        System.out.println("Encrypted Key Size: " + packet.getKeySize());
+        System.out.println("Encrypted Key: " + packet.getKey());
+        System.out.println("Encrypted Binary: " + OneTimePad.decryptBinary(packet.getBinary(), convertDecimalToBinary(packet.getKey(), packet.getKeySize().intValue())));
+
+        System.out.println("Decrypted Text: " + computer2.getRecentMessages().get(0));
+
+    }
+
+    private static int[] convertDecimalToBinary(BigInteger decimal, int keySize) {
+        ArrayList<Integer> binaryList = new ArrayList<>();
+        while (!decimal.equals(BigInteger.ZERO)) {
+            BigInteger[] divideAndRemainder = decimal.divideAndRemainder(BigInteger.TWO);
+            binaryList.add(divideAndRemainder[1].intValue());
+            decimal = divideAndRemainder[0];
+        }
+
+        int[] binary = new int[binaryList.size()];
+        for (int i = 0; i < binary.length; i++) {
+            binary[i] = binaryList.get(binary.length - 1 - i);
+        }
+
+        if (binary.length < keySize) {
+            int[] newBinary = new int[keySize];
+            int difference = keySize - binary.length;
+            for (int i = 0; i < difference; i++) {
+                newBinary[i] = 0;
+            }
+            if (keySize - difference >= 0)
+                System.arraycopy(binary, 0, newBinary, difference, keySize - difference);
+            binary = newBinary;
+        }
+        return binary;
+    }
+}
